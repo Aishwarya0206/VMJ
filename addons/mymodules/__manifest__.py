ea063b7a60d9f4273b5c0994712a9d399c622606
Fahein <aishustarz02@gmail.com>
2020-02-04 18:17:29 +0530

    First commit

---
+++ addons/mymodules/__manifest__.py
@@ -0,0 +1,15 @@
+{
+  'name':'School Theme',
+  'description': 'This is the sample website before building the theme.',
+  'version':'1.0',
+  'author':'Aishwarya',
+
+  'data': [
+        'views/pages.xml',
+	'views/assets.xml',
+	'views/snippets.xml',
+	'views/options.xml',
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['base','website'],
+}
